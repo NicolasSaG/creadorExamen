7aedcf98038834133578185c002a0666a721dc7c
NicolasSaG <fsanchezg1403@alumno.ipn.mx>
2019-10-11 20:31:42 -0500

    Proyecto react creado

---
+++ reactCreadorExamen/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import App from "./js/components/container/Login.jsx";
+
+ReactDOM.render(<App />, document.getElementById("root"));
