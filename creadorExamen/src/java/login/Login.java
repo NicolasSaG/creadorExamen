01c08bd575783703f63564f726a2feedafdfb6c4
NicolasSaG <fsanchezg1403@alumno.ipn.mx>
2019-10-26 18:41:52 -0500

    ejemplo de post y get servlets

---
+++ creadorExamen/src/java/login/Login.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package login;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.ParseException;
+import org.json.simple.parser.JSONParser;
+/**
+ *
+ * @author fnico
+ */
+public class Login extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.setContentType("text/plain");
+        response.getWriter().println("datooos get");
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.setContentType("text/plain");
+        StringBuilder buffer = new StringBuilder();
+        BufferedReader reader = request.getReader();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            buffer.append(line);
+        }
+        String data = buffer.toString();
+        
+        try {
+            Object obj = new JSONParser().parse(data);
+            JSONObject jo = (JSONObject) obj; 
+            Map user = ((Map)jo.get("user")); 
+            response.getWriter().println("usuario: "+user.get("username"));
+            response.getWriter().println("usuario: "+user.get("password"));
+        } catch (ParseException ex) {
+            response.getWriter().println("error:" + ex.getMessage());
+        }
+        response.getWriter().println("datra: //"+data);
+        
+        
+    }
+}
