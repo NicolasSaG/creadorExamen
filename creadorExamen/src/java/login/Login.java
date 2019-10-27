03d9dcfa53998211ac704ea333680d71575f3cfe
NicolasSaG <fsanchezg1403@alumno.ipn.mx>
2019-10-26 19:15:33 -0500

    busqueda de usuarios en archivo xml 2

--- creadorExamen/src/java/login/Login.java
+++ creadorExamen/src/java/login/Login.java
@@ -6,11 +6,13 @@
 package login;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,6 +20,11 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
 import org.json.simple.parser.ParseException;
@@ -46,8 +53,12 @@ public class Login extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String username = "", password = "";
+        HttpSession session = request.getSession();
         response.addHeader("Access-Control-Allow-Origin", "*");
         response.setContentType("text/plain");
+        
+        //convertir body de request en string
         StringBuilder buffer = new StringBuilder();
         BufferedReader reader = request.getReader();
         String line;
@@ -56,17 +67,46 @@ public class Login extends HttpServlet {
         }
         String data = buffer.toString();
         
+        //obtener atributos de string json
         try {
             Object obj = new JSONParser().parse(data);
             JSONObject jo = (JSONObject) obj; 
             Map user = ((Map)jo.get("user")); 
-            response.getWriter().println("usuario: "+user.get("username"));
-            response.getWriter().println("usuario: "+user.get("password"));
+            username = user.get("username").toString();
+            password = user.get("password").toString();
         } catch (ParseException ex) {
-            response.getWriter().println("error:" + ex.getMessage());
+            //response.getWriter().println("error:" + ex.getMessage());
         }
-        response.getWriter().println("datra: //"+data);
         
-        
+        String ruta = request.getRealPath("/");
+        boolean userExists = false;
+        //buscar usaurios en el archivo xml
+        try{
+            SAXBuilder builder = new SAXBuilder();
+            File users = new File(ruta+"users.xml");
+            Document document = builder.build(users);
+            Element root = document.getRootElement();
+            List lista = root.getChildren("usuario");
+                //leer todos los usuarios del archivo
+                for(int i = 0; i < lista.size(); i++){
+                    Element elemento = (Element)lista.get(i);
+                    String xmlUsername = elemento.getAttributeValue("username");
+                    String xmlPassword = elemento.getAttributeValue("password");             
+                    if(username.compareTo(xmlUsername) == 0 && password.compareTo(xmlPassword) == 0){
+                        userExists = true;
+                        session.setAttribute("username", xmlUsername);
+                        break;
+                    } 
+                }
+                if(userExists){
+                    response.getWriter().println("http://localhost:3000/welcome");
+                }else{
+                    response.getWriter().println("http://localhost:3000/greetings");
+                }
+        }
+        catch(JDOMException e){
+            //e.printStackTrace();
+        }  
+        //response.getWriter().println("datra: //"+data);
     }
 }
