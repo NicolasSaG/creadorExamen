daa32c54e451bdc0396120b403cc127c58a10bbb
NicolasSaG <fsanchezg1403@alumno.ipn.mx>
2019-11-03 23:41:49 -0600

    mostrar pregunta hecho

--- reactCreadorExamen/src/js/components/Questions/Questions.js
+++ reactCreadorExamen/src/js/components/Questions/Questions.js
@@ -2,23 +2,35 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./Questions.css";
 import axios from "axios";
+import Modal from "react-modal";
 
 class Questions extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      questions: {}
+      questions: {},
+      open: false,
+      selectedQuestion: {}
     };
     this.getTable();
   }
+  onOpenModal = i => {
+    this.setState({
+      open: true,
+      selectedQuestion: i // cuando se selecciona una pregunta se guarda en el state
+    });
+  };
+
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
 
   getTable() {
     axios.get("http://localhost:8080/creadorExamen/QuestionsServlet").then(
       res => {
         var aux = res.data;
         this.setState({ questions: aux });
-        //console.log(this.state.questions);
       },
       err => alert("2")
     );
@@ -43,7 +55,13 @@ class Questions extends Component {
       children.push(<td className="td-name">{element.text}</td>);
       children.push(
         <td className="td-actions">
-          <button className="btn btn-primary btn-cool">ver</button>
+          <button
+            className="btn btn-primary btn-cool"
+            onClick={() => this.onOpenModal(element)} //pasar pregunta seleccionada
+          >
+            ver
+          </button>
+
           <button className="btn btn-info btn-cool">modificar</button>
           <button className="btn btn-danger btn-cool">eliminar</button>
         </td>
@@ -65,6 +83,96 @@ class Questions extends Component {
             ? console.log("no data")
             : this.createTable()}
         </table>
+        <div>
+          {Object.keys(this.state.selectedQuestion).length === 0 ? (
+            console.log("algo rato")
+          ) : (
+            <Modal
+              isOpen={this.state.open}
+              contentLabel="Minimal Modal Example"
+            >
+              <h3>pregunta: {this.state.selectedQuestion.text}</h3>
+              <p>answer: {this.state.selectedQuestion.answer}</p>
+
+              <div className="row">
+                <div className="col">
+                  <div className="form-group">
+                    <div className="text-center">
+                      <label className="control-label ">drag object name</label>
+                    </div>
+
+                    <p>
+                      drag option 1:{" "}
+                      {this.state.selectedQuestion.drags.option[0].content}
+                      <img
+                        src={`http://localhost:8080/creadorExamen/images/${this.state.selectedQuestion.drags.option[0].img}`}
+                      ></img>
+                    </p>
+                    <p>
+                      drag option 2:
+                      {this.state.selectedQuestion.drags.option[1].content}
+                      <img
+                        src={`http://localhost:8080/creadorExamen/images/${this.state.selectedQuestion.drags.option[1].img}`}
+                      ></img>
+                    </p>
+                    <p>
+                      drag option 3:
+                      {this.state.selectedQuestion.drags.option[2].content}
+                      <img
+                        src={`http://localhost:8080/creadorExamen/images/${this.state.selectedQuestion.drags.option[2].img}`}
+                      ></img>
+                    </p>
+                    <p>
+                      drag option 4:
+                      {this.state.selectedQuestion.drags.option[3].content}
+                      <img
+                        src={`http://localhost:8080/creadorExamen/images/${this.state.selectedQuestion.drags.option[3].img}`}
+                      ></img>
+                    </p>
+                  </div>
+                </div>
+                <div className="col">
+                  <div className="form-group">
+                    <div className="text-center">
+                      <label className="control-label">
+                        target object name
+                      </label>
+                    </div>
+                    <p>
+                      target option 1:
+                      {this.state.selectedQuestion.targets.option[0].content}
+                      <img
+                        src={`http://localhost:8080/creadorExamen/images/${this.state.selectedQuestion.targets.option[0].img}`}
+                      ></img>
+                    </p>
+                    <p>
+                      target option 2:
+                      {this.state.selectedQuestion.targets.option[1].content}
+                      <img
+                        src={`http://localhost:8080/creadorExamen/images/${this.state.selectedQuestion.targets.option[1].img}`}
+                      ></img>
+                    </p>
+                    <p>
+                      target option 3:
+                      {this.state.selectedQuestion.targets.option[2].content}
+                      <img
+                        src={`http://localhost:8080/creadorExamen/images/${this.state.selectedQuestion.targets.option[2].img}`}
+                      ></img>
+                    </p>
+                    <p>
+                      target option 4:
+                      {this.state.selectedQuestion.targets.option[3].content}
+                      <img
+                        src={`http://localhost:8080/creadorExamen/images/${this.state.selectedQuestion.targets.option[3].img}`}
+                      ></img>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <button onClick={this.onCloseModal}>Cerrar pregunta</button>
+            </Modal>
+          )}
+        </div>
       </div>
     );
   }
